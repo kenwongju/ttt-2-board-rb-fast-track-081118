057f429b173d6aed85df4863636901ab5a958fd0
Kenneth Wong <kenwongjune@gmail.com>
2018-08-12 10:19:56 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,13 @@
 # Define the variable board below.
 
-def display_board(board)
-  puts "#{board[0]}, #{board[1]}, #{board[2]}"
-  puts "#{board[3]}, #{board[4]}, #{board[5]}"
-  puts "#{board[6]}, #{board[7]}, #{board[8]}"
+def display_board(
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts 'display board'
+#board output
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
- 
+
