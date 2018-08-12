908d82face1bcc49df2e74cc9386e2048af49a93
Kenneth Wong <kenwongjune@gmail.com>
2018-08-12 10:20:58 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,7 @@
 # Define the variable board below.
 
 def display_board (board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-  puts 'display board'
-#board output
+puts "display board"
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
