3f0b45d623201342078850822070057d5aabbdfe
Kenneth Wong <kenwongjune@gmail.com>
2018-08-12 10:05:00 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,14 @@
 # Define the variable board below.
+
+def board = row ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+ row[0]
+ row[1]
+ row[2]
+ row[3]
+ row[4]
+ row[5]
+ row[6]
+ row[7]
+ row[8]
+ 
+ end
